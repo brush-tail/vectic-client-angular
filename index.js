0d998049144d68b7003f056d686f85ff137cfa49
Buildkite <evan@mylocalbroker.com.au>
2016-02-09 16:56:40 +1100

    Added vectic provider and directive with config functions

---
+++ index.js
@@ -0,0 +1,45 @@
+angular.app('vectic')
+.provider('vectic', function vecticProvider() {
+  
+  var _host = '';
+  var _path = '/api/vectic/';
+
+  function Generator() {
+    var _this = this;
+
+    Object.defineProperties(this, {
+      path: {
+        get: function() { return _path || ''; },
+      },
+      host: {
+        get: function() { return _host || ''; },
+      },
+    });
+  }
+
+  // Configuation functions
+
+  this.setPath = function(path) {
+    _path = path;
+  }
+  this.setHost = function(host) {
+    _host = host;
+  }
+
+  this.$get = [function vecticGenerator() {
+    return new Generator();
+  }];
+})
+.directive('vectic', ['vectic', function(vectic) {
+  // TODO: In Progress...
+  return {
+    // template: '<div class="svgContainer"></div>',
+    template: '<object ng-attr-data="{{filepath}}"></object>',
+    restrict: 'E',
+    scope: {
+      id: '=?',
+    },
+    link: function($scope, $element, $attr) {
+      $scope.filepath = vectic.path+$scope.id;
+    },
+});
